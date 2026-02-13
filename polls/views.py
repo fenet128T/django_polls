44e042e3952593595ede1fe7b4edc25769286b5b
fenet128t <fenet128t@gmail.com>
2026-02-14 01:48:14 +0300

    fix mysite's setting and polls's urls for correct path

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import Http404
-
-from .models import Question
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Question, Choice
 
 
 
@@ -15,12 +16,28 @@ def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+        return get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     response = "You're looking at the results of question %s."
-    return render(request, "polls/results.html", {"response": response % question_id})
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return render(request, "polls/vote.html", {"question_id": question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
